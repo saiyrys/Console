300b59e5d1268ba5028d682b462be9a48f119ddf
saiyrys <135985271+saiyrys@users.noreply.github.com>
2023-06-25 22:32:09 +0300

    Task 4.2

---
+++ Program 4.2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_4._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число:");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = CalculateDigitSum(number);
+
+            Console.WriteLine("Сумма цифр числа: " + sum);
+
+            Console.ReadLine();
+        }
+        static int CalculateDigitSum(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
