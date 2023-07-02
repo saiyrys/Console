dc20f171a8cd0d9e92f65bf69e8ae66503ce71c7
saiyrys <135985271+saiyrys@users.noreply.github.com>
2023-07-02 23:49:04 +0300

    Task 6.1

---
+++ Program6.1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько чисел вы хотите ввести: ");
+
+            int M = int.Parse(Console.ReadLine());
+
+            int[] valueNumbers = new int[M];
+
+            for (int i = 0; i < M; i++)
+            {
+                Console.Write("Введите ваше число: ");
+                valueNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int result = CountNumbers(valueNumbers);
+            Console.WriteLine($"Чисел больше нуля: {result}");
+        }
+        static int CountNumbers (int[] numbers)
+        {
+            int count = 0;
+
+            foreach(int num in numbers)
+            {
+                if (num > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
