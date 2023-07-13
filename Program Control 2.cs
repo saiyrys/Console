37b0913ad4d3a4c28b9367803419b9053aae0464
saiyrys <135985271+saiyrys@users.noreply.github.com>
2023-07-13 22:17:01 +0300

    Control Task 2

---
+++ Program Control 2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlling_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите два неотрицательных числа m и n для вычисления функции Аккермана");
+            Console.Write("Введите число m: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите число n: ");
+            int n = int.Parse(Console.ReadLine()); 
+
+            int result = AckermannFunction(m, n);
+            Console.WriteLine($"A({m}, {n}) = {result}");
+        }
+
+        static int AckermannFunction(int m, int n)
+        {
+            if (m == 0)
+                return n + 1;
+            else if (n == 0)
+                return AckermannFunction(m - 1, 1);
+            else
+                return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
